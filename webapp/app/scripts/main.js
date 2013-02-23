a18ce78df43f372c5db3621e96bdaec5aa59d0e0
Hasen Ahmad <kaizokuace@gmail.com>
2013-02-23 03:17:33 -0800

    sorted out yeoman crap

--- webapp/app/scripts/main.js
+++ webapp/app/scripts/main.js
@@ -1,14 +1,15 @@
+require.config({
+  shim: {
+  },
 
-window.webapp = {
-  Models: {},
-  Collections: {},
-  Views: {},
-  Routers: {},
-  init: function() {
-    console.log('Hello from Backbone!');
+  paths: {
+    hm: 'vendor/hm',
+    esprima: 'vendor/esprima',
+    jquery: 'vendor/jquery.min'
   }
-};
-
-$(document).ready(function(){
-  webapp.init();
+});
+ 
+require(['app'], function(app) {
+  // use app here
+  console.log(app);
 });

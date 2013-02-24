672b942e87e16ba2ce73bb0e23832fcc2b76219b
George Mathews <georgeQgeorgemathe.ws>
2013-02-24 01:13:50 -0800

    use nodemon instead of node

--- provisioning/startnode/recipes/default.rb
+++ provisioning/startnode/recipes/default.rb
@@ -7,9 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 # Start the chosen node app with a chosen port
-execute "start-node" do
-    user "www-data"
-    not_if( "ps l -Cnode|grep #{node[:startnode][:start_file]} #{node[:startnode][:port]}" )
-    command "node #{node[:startnode][:start_file]} #{node[:startnode][:port]} &"
+bash "start-node" do
+  user "www-data"
+  code <<-EOH
+    nodemon #{node[:startnode][:start_file]} #{node[:startnode][:port]} &
+    EOH
+    not_if "ps l -Cnode|grep #{node[:startnode][:start_file]} #{node[:startnode][:port]}"
 end
 
